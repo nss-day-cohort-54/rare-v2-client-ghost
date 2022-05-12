03cd9db1123a80b3b6f901d559ab9c633d8f4739
Noah Taggart <noahtag@gmail.com>
2022-05-12 10:58:27 -0500

    added create tags function

--- src/components/categories/AllCategories.js
+++ src/components/categories/AllCategories.js
@@ -1,39 +1,29 @@
-// imports
-// import getAllCategories from /.CategoryManager 
 import { getAllCategories } from "./CategoryManager";
 import React, { useEffect, useState } from "react";
 import { NewCategoryForm } from "./CreateCategoryForm";
-// import React, useEffect, useState 
+
 
 // declare and export function AllCategories which get all category objects
 
-export const AllCategories = () => {
-// use UseState to set the state for the categories array for
-// when the state changes.
+export const AllCategories = ({refreshState, setRefreshState}) => {
+
     const [categories, setCategories] = useState([])
-    const [refreshState, setRefreshState] = useState(false)
+
     
 
     // use UseEffect to getAllCategories and set the state of the category array.
     useEffect(() => {
-        getCategories()
-        setRefreshState(false)
+        getAllCategories()
+        .then(data => setCategories(data))
+        .then(setRefreshState(false))
     },
     [refreshState])
-    
-    const getCategories = () => {
-        getAllCategories()
-            .then((categories) => {
-                setCategories(categories)
-            })
-        
 
-    }
 
 // return a map through the categories array that will have 
 // edit and delete buttons  
     return <>
-        <div>AllCategories Page</div>
+        <div>All Categories</div>
         {categories.map((category) => {
             return <div key={`category--${category.id}`}>{category.label}
                 <button>edit</button> <button>delete</button>
@@ -42,8 +32,6 @@ export const AllCategories = () => {
         <div className="CreateNewCategoryFormContainer">
             <NewCategoryForm setRefreshState={setRefreshState} />
         </div>
-
-
     </>
 }
 
