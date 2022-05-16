791c152a407f2492d8a988b7cea6b047d483d0aa
Randall Thomas <randallthomasmusic@gmail.com>
2022-05-16 08:33:42 -0700

    tags delete in order

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -1,30 +1,46 @@
 import { getAllTags } from "./TagManager"
 import React, { useEffect, useState } from "react";
 import { NewTagForm } from "./CreateTagForm";
+import { getCurrentUser } from "../users/UserManager";
+import { ButtonControls } from "../buttonControls/ButtonControls";
 
 
 
 export const AllTags = ({refreshState, setRefreshState}) => {
 
     const [tags, setTags] = useState([])
-
+    const [currentUser, setUser] = useState({})
     // use UseEffect to getAllTags and set the state of the tag array.
+
     useEffect(() => {
-        getAllTags()
-        .then(data => setTags(data))
-        .then(setRefreshState(false))
-    },
-    [refreshState])
+        getCurrentUser()
+            .then((data) => setUser(data))
+            .then(getAllTags)
+            .then(data => setTags(data))
+            .then(setRefreshState(false))
+        },
+        [refreshState])
 
     return <>
         <div>All Tags</div>
         {tags.map((tag) => {
-            return <div key={`tag--${tag.id}`}>{tag.label} 
-            <button>edit</button> <button>delete</button>
-            </div>
+            
+            return <div id={`tag--${tag.id}`}>{tag.label} 
+                    {
+                        currentUser.is_staff
+                        ? <div className="cardButtons">
+                            <ButtonControls 
+                                isTags={true} 
+                                id={tag.id} 
+                                setRefreshState={setRefreshState} />
+                            </div>
+                        : null
+                    }
+                </div>
         })}
         <div className="CreateNewTagFormContainer">
             <NewTagForm setRefreshState={setRefreshState} />
         </div>
     </>
 }
+
