0a6120059e253fca12a37bfa1adb4c09a8f60a01
Randall Thomas <randallthomasmusic@gmail.com>
2022-05-17 11:04:59 -0700

    needed

--- src/components/buttonControls/ButtonControls.js
+++ src/components/buttonControls/ButtonControls.js
@@ -4,23 +4,25 @@ import { deletePost } from "../posts/PostManager"
 import { useHistory } from "react-router-dom"
 import { deleteTag, sendTagEdit } from "../tags/TagManager"
 import { useEffect, useState } from "react"
+import { deleteCategory, editCategory } from "../categories/CategoryManager"
 
-export const ButtonControls = ({ isPost, id, commentId, getComments, isTags, setRefreshState, tag }) => {
+export const ButtonControls = ({ isPost, id, commentId, getComments, isTags, setRefreshState, tag, isCategories, category }) => {
   const history = useHistory()
   const [singleTag, setSingleTag] = useState()
   useEffect(() => {
     setSingleTag(tag)
-}, [tag])
+  }, [tag])
 
   return <div>
     <dialog id={`anything-${id}`}>
-      
+
       {
         isPost ? <div>Are you sure you want to delete this post?</div>
-        : isTags ? <div>Are you sure you want to delete this tag?</div>
-        : <div>Are you sure you want to delete this comment?</div>
+          : isTags ? <div>Are you sure you want to delete this tag?</div>
+            : isCategories ? <div>Are you sure you want to delete this category?</div>
+              : <div>Are you sure you want to delete this comment?</div>
       }
-      
+
       <div>
         <button
           onClick={
@@ -34,6 +36,18 @@ export const ButtonControls = ({ isPost, id, commentId, getComments, isTags, set
                     })
               } else if (isTags) {
                 deleteTag(id)
+                  .then(
+                    () => {
+                      const buttonTarget = document.querySelector(`#anything-${id}`)
+                      buttonTarget.close()
+                    }
+                  )
+                  .then(
+                    () => {
+                      setRefreshState(true)
+                    })
+              } else if (isCategories) {
+                deleteCategory(id)
                   .then(
                     () => {
                       const buttonTarget = document.querySelector(`#anything-${id}`)
@@ -74,20 +88,33 @@ export const ButtonControls = ({ isPost, id, commentId, getComments, isTags, set
       </div>
 
     </dialog>
+    {/* -------------edit button------------- */}
     <button onClick={() => {
-      if(isPost) {
+      if (isPost) {
         history.push(`/editPost/${id}`)
-      } else if(isTags) {
-        // prompt allows user to edit tag label
-          const val = prompt("Edit Tag:", tag.label)
-          // prompt returns null if cancel button is pressed
-          if (val !== null) {
+      } else if (isTags) {
+        // prompt allows user to enter info
+        // tag.label is set as initial state for editing
+        const val = prompt("Edit Tag:", tag.label)
+        // prompt returns null if cancel button is pressed
+        if (val !== null) {
           const newTag = {
-            id:tag.id,
-            label:val
+            id: tag.id,
+            label: val
           }
           sendTagEdit(newTag)
-          .then(setRefreshState(true))
+            .then(setRefreshState(true))
+        }
+      } else if (isCategories) {
+        // prompt state initialized with category label to edit
+        const val = prompt("Edit Category:", category.label)
+        if (val !== null) {
+          const categoryEdit = {
+            id: category.id,
+            label: val
+          }
+          editCategory(categoryEdit)
+            .then(setRefreshState(true))
         }
       } else {
         window.alert("Cannot edit comments")
