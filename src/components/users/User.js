88a8c6e8907d2a00baf9da5cf982213ed07b042e
Randall Thomas <randallthomasmusic@gmail.com>
2022-05-18 07:34:13 -0700

    user details

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -40,25 +40,7 @@ export const User = ({ listView, user }) => {
             }
         }, [viewUser]
     )
-        // define state variables
-        // maybe get user's articles for the clickable article count?
-        // articles, setArticles = useState()
-        // subscribed, setSubscribed = useState(false) // default could be false
-
-    // define useEffects
-    // only needed for list view
-    // useEffect(() => getArticlesForUser function then setArticles, [])
-
-    /* useEffect(() => getSubscribedStatus)
-        this useEffect can run on page load 
-        needs to check if the viewing user is subscribed to the viewed user
-        get subscribed list from database
-            the database function should probably take id as param
-            only returns subbed relationships of the viewing user
-        iterate over the sub list 
-            to check if viewed user is in the list
-            if viewer is subbed to viewed setSubscribed state to true
-    */
+        
     // does subscribe button need an onclick?
         // yes
         // if subbed - onclick calls delete sub function
@@ -79,14 +61,14 @@ export const User = ({ listView, user }) => {
             </div> 
             : viewUser
                 ? <div>
-                    <div>Picture: <img src={`${viewUser.profileImageUrl || "https://m.media-amazon.com/images/I/91xDQaUMubS._AC_SL1500_.jpg"}`} width={300} height={300} /></div>
-                    <div>Name: {viewUser.firstName} {viewUser.lastName}</div>
-                    <div>Username: {viewUser.username}</div>
-                    <div>Email: {viewUser.email}</div>
-                    <div>Creation Date: {viewUser.createdOn}</div>
-                    <div>Profile Type: Author</div>
+                    <div>Picture: <img src={`${viewUser.user.profileImageUrl || "https://www.themoviedb.org/t/p/w235_and_h235_face/j2De8KaACIbi4IX8WfUZGmCW1k2.jpg"}`} width={300} height={300} /></div>
+                    <div>Name: {viewUser.user.firstName} {viewUser.user.lastName}</div>
+                    <div>Username: {viewUser.user.username}</div>
+                    <div>Email: {viewUser.user.email}</div>
+                    <div>Creation Date: {viewUser.user.date_joined}</div>
+                    <div>Profile Type: {viewUser.user.is_staff ? "Admin" : "Author"}</div>
                     <div>
-                        <Link to={`/posts/user/${viewUser.id}`}>
+                        <Link to={`/posts/user/${viewUser.user.id}`}>
                         See Articles - Count: {postCount}
                         </Link>
                     </div>
@@ -96,20 +78,6 @@ export const User = ({ listView, user }) => {
                 </div>
                 : null
         }
-    {/* 
-        JSX for the individual user
-            in list form - just need name and link to individual page
-
-            in single view - see wireframe
-                - image
-                - first name last name
-                - username
-                - email
-                - creation date
-                - profile type
-                - clickable article count
-                - subscribe button - displays as either subscribe or unsubscribe
-    */}
     
     </>
 }
